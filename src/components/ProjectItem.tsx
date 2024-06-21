43dce22363daa4bdc6e97a5b827fe20205af8df5
Hongyu Zeng <zdszym@me.com>
2024-06-21 14:10:39 +0800

    project item UI synced

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 import { Project } from "./Dashboard";
+import { Avatar, Button } from "antd";
+import projectItemStyles from "./ProjectItem.module.css";
 
 interface ProjectItemProps {
   project: Project;
 }
 
+// todos: replace hardcoded color with mapping with respect to the category
+const getColorBarColorBasedOnCategory = (category: string): string => {
+  return "green";
+};
+
+const getAvatarTextColorBasedOnCategory = (category: string): string => {
+  return "blue";
+};
+
+const getAvatarBackgroundColorBasedOnCategory = (category: string): string => {
+  return "grey";
+};
+
 const ProjectItem = ({ project }: ProjectItemProps) => {
-  return <div>{project.name}</div>;
+  return (
+    <div className={projectItemStyles.container}>
+      <div
+        className={projectItemStyles.colorBar}
+        style={{
+          backgroundColor: getColorBarColorBasedOnCategory(project.category),
+        }}
+      ></div>
+      <div className={projectItemStyles.details}>
+        <Avatar
+          className={projectItemStyles.avatar}
+          style={{
+            backgroundColor: getAvatarBackgroundColorBasedOnCategory(
+              project.category
+            ),
+            color: getAvatarTextColorBasedOnCategory(project.category),
+          }}
+          size={28}
+        >
+          {project.category}
+        </Avatar>
+        <p className={projectItemStyles.title}>{project.name}</p>
+        <p className={projectItemStyles.user}>{`${project.users} users`}</p>
+        <div className={projectItemStyles.dashboard}>
+          <span
+            className={projectItemStyles.dashboardText}
+          >{`${project.dashboards} dashboards`}</span>
+        </div>
+        <Button className={projectItemStyles.deleteButton} type="text" danger>
+          Delete
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectItem;
